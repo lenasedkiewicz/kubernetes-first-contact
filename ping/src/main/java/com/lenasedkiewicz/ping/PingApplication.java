f56fc85140c589da569497f7420ba3398d88960a
msedkiewicz <magdalenainspirations@gmail.com>
2026-01-05 21:10:36 +0100

    Add ping endpoint

--- ping/src/main/java/com/lenasedkiewicz/ping/PingApplication.java
+++ ping/src/main/java/com/lenasedkiewicz/ping/PingApplication.java
@@ -2,12 +2,31 @@ package com.lenasedkiewicz.ping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class PingApplication {
+
+    private WebClient webClient = WebClient.create();
 
 	public static void main(String[] args) {
 		SpringApplication.run(PingApplication.class, args);
 	}
 
+    // tag::ping-endpoint[]
+    @GetMapping
+    public Mono<String> index() {
+        return webClient.get().uri("http://gs-spring-boot-k8s/helloworld")
+                .retrieve()
+                .toEntity(String.class)
+                .map(entity -> {
+                    return entity.getBody();
+                });
+    }
+
+    // end::ping-endpoint[]
 }
